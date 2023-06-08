3a5c55b965a39359773d4c8511484642d1291e9e
Eduard Sargsyan <eduard.sarkisyan@gmail.com>
2023-06-08 21:17:21 +0200

    Selecting buttons on Dialog and handling presses

--- include/ui_dialog.h
+++ include/ui_dialog.h
@@ -1,14 +1,20 @@
 #pragma once
 
 #include "ui_screen.h"
+#include <functional>
 
 enum class dialog_buttons_t
 {
+    none = 0,
     ok = 1 << 0,
     cancel = 1 << 1,
     yes = 1 << 2,
     no = 1 << 3,
-    abort = 1 << 4
+    next = 1 << 4,
+    previous = 1 << 5,
+    abort = 1 << 6,
+    accept = 1 << 7,
+    dismiss = 1 << 8
 };
 
 class Dialog: public Screen
@@ -20,8 +26,14 @@ public:
     inline const char* text() const { return m_text; }
     inline uint8_t buttons() const { return m_buttons; }
     inline void setButtons(uint8_t buttons) { m_buttons = buttons; }
+    inline dialog_buttons_t selectedButton() const { return m_selected; }
+    virtual bool handle(const user_input_t key, const UIStyle& style);
+    std::function<void(dialog_buttons_t)> on_result = nullptr;
+private:
+    void setNextButton(uint8_t direction);
 private:
     uint8_t m_buttons = 0;
+    dialog_buttons_t m_selected = (dialog_buttons_t)0;
     const char* m_text = "";
 };
 
--- src/ui_dialog.cpp
+++ src/ui_dialog.cpp
@@ -4,3 +4,53 @@ Dialog::Dialog()
 {
     type = element_type_t::dialog;
 }
+
+bool Dialog::handle(const user_input_t key, const UIStyle& style)
+{
+    int dir = 0;
+    if (key == user_input_t::key_increment) dir += 1;
+    if (key == user_input_t::key_decrement) dir -= 1;
+
+    if (dir == 0 && selectedButton() == dialog_buttons_t::none) setNextButton(1);
+    else if (dir != 0) setNextButton(dir);
+    
+    if (key == user_input_t::key_pressed || key == user_input_t::key_long_pressed)
+    {
+        if (on_result != nullptr)
+            on_result(m_selected);
+    }
+
+    return true;
+}
+
+void Dialog::setNextButton(uint8_t direction)
+{
+    uint8_t bits_count = (sizeof(dialog_buttons_t) * 8);
+    uint16_t candidate = (uint16_t)m_selected;
+    uint16_t bit = 0;
+
+    for(int i = (bits_count -1); i >  -1; --i)
+    {
+        if (candidate & (1 << i))
+        {
+            bit = i;
+            break;
+        }
+    }
+
+    for(int i = 1; i < bits_count; ++i)
+    {
+        if (direction > 0)
+        {
+            candidate = 1 << ((bit + i) % bits_count);
+        } else if (direction < 0)
+        {
+            candidate = 1 << ((bit - i) % bits_count);
+        }
+        if (candidate & m_buttons)
+        {
+            m_selected = (dialog_buttons_t)(candidate);
+            break;
+        }
+    }
+}
